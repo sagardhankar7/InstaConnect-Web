db37b1c2234664fdaae06e584a863ec10b5cf6dc
Sagar Dhankar <sag9315@gmail.com>
2026-04-04 13:51:46 +0530

    Add optional chaining to user properties in Profile component for safer rendering on Reload

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -9,13 +9,15 @@ const Profile = () => {
     <div className="flex justify-center my-5">
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure>
-          <img src={user.photoUrl} alt="Profile photo" />
+          <img src={user?.photoUrl} alt="Profile photo" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{user.firstName + " " + user.lastName}</h2>
-          <p>{user.about}</p>
-          <p>{user.skills}</p>
-          <p>{user.email}</p>
+          <h2 className="card-title">
+            {user?.firstName + " " + user?.lastName}
+          </h2>
+          <p>{user?.about}</p>
+          <p>{user?.skills}</p>
+          <p>{user?.email}</p>
           <div className="card-actions justify-end">
             {/* <button className="btn btn-primary"></button> */}
           </div>
