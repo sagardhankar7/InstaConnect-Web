ddc877be0d1654f2b67e1760c6dd1c624e7fa53a
Sagar Dhankar <sag9315@gmail.com>
2026-04-04 09:34:03 +0530

    Add user slice and configure store for Redux state management

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import user from "../features/user/userSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: user,
+  },
 });
 
 export default store;
