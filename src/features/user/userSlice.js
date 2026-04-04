92996f7d27b594c718e49b874b4ca3e2703a7a8b
Sagar Dhankar <sag9315@gmail.com>
2026-04-04 11:45:12 +0530

    user slice with reducers for adding and removing user state

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,8 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
   name: "user",
-  initialState: null,
-  reducers: {},
+  initialState: null, //initially user null
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload; // update state
+    },
+    removeUser: (state, action) => {
+      return null; // update state
+    },
+  },
 });
 
 export default userSlice.reducer;
+export const { addUser, removeUser } = userSlice.actions;
