cca960de9dacd5a62b426a994011a28b72851d5c
Sagar Dhankar <sag9315@gmail.com>
2026-04-05 09:04:01 +0530

    Implement feed fetching and rendering in Feed component

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "./utils/constants";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [data, setData] = useState([]);
+
+  const handleFeed = async () => {
+    const res = await axios.get(BASE_URL + "/feed", { withCredentials: true });
+    console.log(res.data);
+    console.log(Array.isArray(res.data));
+    setData(res.data);
+  };
+  useEffect(() => {
+    handleFeed();
+  }, []);
+
+  return (
+    <>
+      {data.map((user) => (
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <figure className="px-10 pt-10">
+            <img src={user.photoUrl} alt="user" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              {user.firstName} {user.lastName}
+            </h2>
+            <p>{user.about}</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Like</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Feed;
