452066aa199dbe4c238337ddb184d2161aba759b
sagar dhankar <92321286+sagardhankar7@users.noreply.github.com>
2026-06-24 16:37:06 +0530

    Update BASE_URL to new backend endpoint

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://insta-connect-backend.vercel.app"
+export const BASE_URL = "https://instaconnect-backend2.onrender.com"
+  //"https://insta-connect-backend.vercel.app"
   //  "https://instaconnect-backend-n6ox.onrender.com";
 
 //   "http://localhost:7744";
